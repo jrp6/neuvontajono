ffcabca1323f9c7a654c6fcbb44b560c3e3d06d2
Juho Rinta-Paavola <juho.rinta-paavola@aalto.fi>
2018-02-20 13:47:28 +0200

    Handle ordinal numbers correctly

--- keystone.js
+++ keystone.js
@@ -5,6 +5,18 @@ swig.setDefaults({
   cache : false
 });
 
+swig.setFilter('ordinal', (number) => {
+  if (number % 10 === 1 && number % 100 !== 11) {
+    return number + 'st'
+  } else if (number % 10 === 2 && number % 100 !== 12) {
+    return number + 'nd'
+  } else if (number % 10 === 3 && number % 100 !== 13) {
+    return number + 'rd'
+  } else {
+    return number + 'th'
+  }
+})
+
 keystone.init({
 
   'name' : 'Neuvontajono',
